1fd087b176183402038eb332d9cc9e38124d52e4
shariefj <shariefj@users.noreply.github.com>
2019-12-25 21:55:34 -0500

    added path variable to category details

--- silverDemo/src/main/java/com/silver/commerce/controllers/DataReviewController.java
+++ silverDemo/src/main/java/com/silver/commerce/controllers/DataReviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,8 +49,9 @@ public class DataReviewController {
 		return catalogServicesImpl.fetchRootCategories();
 	}
 	
-	@GetMapping("/details")
-	public List<Category> getChild(@RequestParam("categoryId") int categoryId ) {
+	//PathVariable example
+	@GetMapping("/details/{categoryId}")
+	public List<Category> getChild(@PathVariable int categoryId ) {
 		
 		return catalogServicesImpl.fetchChildCategories(categoryId);
 	}
