079c88a073d418b845d5084665ee41ae52b7a658
shariefj <shariefj@users.noreply.github.com>
2019-12-25 21:02:10 -0500

    Changed default Datasource to use  HikariCP.

--- silverDemo/src/main/java/com/silver/commerce/config/JpaConfig.java
+++ silverDemo/src/main/java/com/silver/commerce/config/JpaConfig.java
@@ -9,13 +9,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
  
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.silver.commerce"})
@@ -27,22 +29,27 @@ public class JpaConfig {
 	        = new LocalContainerEntityManagerFactoryBean();
 	      em.setDataSource(dataSource());
 	      em.setPackagesToScan(new String[] { "com.silver.commerce" });
-	 
 	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 	      em.setJpaVendorAdapter(vendorAdapter);
 	      em.setJpaProperties(additionalProperties());
-	 
 	      return em;
 	   }
       
 	  
 	  @Bean
 	  public DataSource dataSource(){
-	      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	      dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	      dataSource.setUrl("jdbc:mysql://localhost:3306/commerce?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-	      dataSource.setUsername( "silver_commerce" );
-	      dataSource.setPassword( "silver_commerce" );
+		  HikariConfig hikariConfig = new HikariConfig();
+		  hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/commerce?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
+		  hikariConfig.setUsername("silver_commerce");
+		  hikariConfig.setPassword("silver_commerce");
+		  hikariConfig.addDataSourceProperty("cachePrepStmts" ,"true");
+		  hikariConfig.addDataSourceProperty("prepStmtCacheSize" ,"250");
+		  hikariConfig.addDataSourceProperty( "prepStmtCacheSqlLimit","2048");
+		  hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		  hikariConfig.setConnectionTimeout(30000);
+		  hikariConfig.setIdleTimeout(60000);
+		  hikariConfig.setMaxLifetime(1800000);
+		  HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 	      return dataSource;
 	  }
 	  
@@ -51,7 +58,6 @@ public class JpaConfig {
 	  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
 	      JpaTransactionManager transactionManager = new JpaTransactionManager();
 	      transactionManager.setEntityManagerFactory(emf);
-	   
 	      return transactionManager;
 	  }
 	   
@@ -62,12 +68,11 @@ public class JpaConfig {
 	   
 	  Properties additionalProperties() {
 	      Properties properties = new Properties();
-	     properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-	 //     properties.setProperty("hibernate.show_sql", "true");
-	      properties.setProperty("hibernate.format_sql", "true");
-	   //   properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+	      properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+	   // properties.setProperty("hibernate.show_sql", "true");
+	   // properties.setProperty("hibernate.format_sql", "true");
+	   // properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 	      properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-	          
 	      return properties;
 	  }
 }
